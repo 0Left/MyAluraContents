6b515966ee3b1eb1a58d84f4177633ddb2b1aab8
0Left <pedro.neto@pwc.com>
2022-05-09 22:30:11 -0300

    Exercicio 01

---
+++ Javascript/Curso01 - Ling na Web/MyContent/Nutricao_Site/js/principal.js
@@ -0,0 +1,4 @@
+var titulo = document.querySelector(".titulo");
+var dateTime = new Date();
+titulo.textContent = "Aparecida Nutricionista || " + dateTime.getHours() + ":" + dateTime.getMinutes();
+
