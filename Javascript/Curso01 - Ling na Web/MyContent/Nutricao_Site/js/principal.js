7d2605240f196edd3b4f4baa3bba23a4ffa8a175
0Left <pedro.neto@pwc.com>
2022-05-09 22:56:22 -0300

    Calculando o IMC

--- Javascript/Curso01 - Ling na Web/MyContent/Nutricao_Site/js/principal.js
+++ Javascript/Curso01 - Ling na Web/MyContent/Nutricao_Site/js/principal.js
@@ -2,3 +2,12 @@ var titulo = document.querySelector(".titulo");
 var dateTime = new Date();
 titulo.textContent = "Aparecida Nutricionista || " + dateTime.getHours() + ":" + dateTime.getMinutes();
 
+
+let listOfPatients = document.querySelectorAll(".paciente");
+for(let i = 0; i < listOfPatients.length; i++)
+{
+    let pesoTocalc = listOfPatients[i].querySelector(".info-peso").textContent;
+    let alturaToCalc = listOfPatients[i].querySelector(".info-altura").textContent;
+    let imc = pesoTocalc / (alturaToCalc * alturaToCalc);
+    listOfPatients[i].querySelector(".info-imc").textContent = imc;
+}
