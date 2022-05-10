d461eb616d134ced133e2a361194b4960b590470
0Left <pedro.neto@pwc.com>
2022-05-10 09:42:09 -0300

    Arredondando IMC

--- Javascript/Curso01 - Ling na Web/MyContent/Nutricao_Site/js/principal.js
+++ Javascript/Curso01 - Ling na Web/MyContent/Nutricao_Site/js/principal.js
@@ -1,13 +1,22 @@
 var titulo = document.querySelector(".titulo");
 var dateTime = new Date();
-titulo.textContent = "Aparecida Nutricionista || " + dateTime.getHours() + ":" + dateTime.getMinutes();
-
-
+titulo.textContent = "Aparecida Nutricionista || " + dateTime.getHours() + ":" + dateTime.getMinutes() + ":"+ dateTime.getSeconds();
+/* 
+* https://www.codingem.com/javascript-how-to-limit-decimal-places/#:~:text=To%20limit%20decimal%20places%20in%20JavaScript%2C%20use%20the%20toFixed(),Converts%20it%20into%20a%20string.
+* Ler o que ta ai depois para entender melhor essa função antes de sair usando, mas ela ta certa
+Number.prototype.round = function(n) {
+    const d = Math.pow(10, n);
+    return Math.round((this + Number.EPSILON) * d) / d;
+}
+*/
 let listOfPatients = document.querySelectorAll(".paciente");
 for(let i = 0; i < listOfPatients.length; i++)
 {
     let pesoTocalc = listOfPatients[i].querySelector(".info-peso").textContent;
     let alturaToCalc = listOfPatients[i].querySelector(".info-altura").textContent;
     let imc = pesoTocalc / (alturaToCalc * alturaToCalc);
-    listOfPatients[i].querySelector(".info-imc").textContent = imc;
+    listOfPatients[i].querySelector(".info-imc").textContent = imc.toFixed(2);
 }
+
+
+
